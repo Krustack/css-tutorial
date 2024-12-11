413b496f4cd53efde1732c0c51f1aefd71747c76
wisanu <wisanu.pho@cru.ac.th>
2024-12-11 13:07:05 +0700

    border-radius

--- basic-box/script.js
+++ basic-box/script.js
@@ -8,7 +8,24 @@ const bgColor = document.querySelector('#bg input');
 const borderSizes = document.querySelector('#border #line-size');
 const borderStyles = document.querySelector('#border #line-style');
 const borderColors = document.querySelector('#border #line-color');
+const borderRadius = document.querySelector('#border-radius');
+const radiusMode = document.querySelector('#radius-mode');
 const box = document.querySelector('#box-el');
+
+function updateRadius() {
+  const radius1 = document.querySelector('#border-radius > div > div:nth-child(5) > input');
+  const radius2 = document.querySelector('#border-radius > div > div:nth-child(6) > input');
+  const radius3 = document.querySelector('#border-radius > div > div:nth-child(7) > input');
+  const radius4 = document.querySelector('#border-radius > div > div:nth-child(8) > input');
+  if(radiusMode.value === 'all') {
+    box.style.borderRadius = radius1.value + 'px';
+  }else if(radiusMode.value === 'y-x') {
+    box.style.borderRadius = radius1.value + 'px ' + radius2.value + 'px';
+  }else if(radiusMode.value === 'stand-alone') {
+    box.style.borderRadius = radius1.value + 'px ' + radius2.value + 'px ' + radius3.value + 'px ' + radius4.value + 'px';
+  }
+  
+}
 
 function updateBox() {
   if(widthUnit.value === 'rem' || heightUnit.value === 'rem') {
@@ -33,6 +50,48 @@ function updateHTMLFontSize() {
   document.documentElement.style.fontSize = HTMLFontSizes.value + 'px';
 }
 
+function updateRadiusMode() {
+  const p1 = document.querySelector('#border-radius > div > p:nth-child(1)');
+  const p2 = document.querySelector('#border-radius > div > p:nth-child(2)');
+  const p3 = document.querySelector('#border-radius > div > p:nth-child(3)');
+  const p4 = document.querySelector('#border-radius > div > p:nth-child(4)');
+  const input1 = document.querySelector('#border-radius > div > div:nth-child(5)');
+  const input2 = document.querySelector('#border-radius > div > div:nth-child(6)');
+  const input3 = document.querySelector('#border-radius > div > div:nth-child(7)');
+  const input4 = document.querySelector('#border-radius > div > div:nth-child(8)');
+  console.log(radiusMode.value);
+  console.log(input1);
+  if(radiusMode.value === 'all') {
+    p1.textContent = 'radius';
+    p2.textContent = '';
+    p3.textContent = '';
+    p4.textContent = '';
+    input1.style.display = 'inline-block';
+    input2.style.display = 'none';
+    input3.style.display = 'none';
+    input4.style.display = 'none';
+  }
+  if(radiusMode.value === 'y-x') {
+    p1.textContent = 'x';
+    p2.textContent = 'y';
+    p3.textContent = '';
+    p4.textContent = '';
+    input1.style.display = 'inline-block';
+    input2.style.display = 'inline-block';
+    input3.style.display = 'none';
+    input4.style.display = 'none';
+  }
+  if(radiusMode.value === 'stand-alone') {
+    p1.textContent = 'top-left';
+    p2.textContent = 'top-right';
+    p3.textContent = 'bottom-right';
+    p4.textContent = 'bottom-left';
+    input1.style.display = 'inline-block';
+    input2.style.display = 'inline-block';
+    input3.style.display = 'inline-block';
+    input4.style.display = 'inline-block';
+  }
+}
 
 inputWidth.addEventListener('input', updateBox);
 widthUnit.addEventListener('change', updateBox);
@@ -44,3 +103,5 @@ boxFontSize.addEventListener('input', updateBox);
 borderSizes.addEventListener('input', updateBox);
 borderStyles.addEventListener('change', updateBox);
 borderColors.addEventListener('input', updateBox);
+radiusMode.addEventListener('change', updateRadiusMode);
+borderRadius.addEventListener('input', updateRadius);
